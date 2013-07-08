2e1adbdfaac8431282b1104aad2819c5b1d03aaa
liuxingqi <lxq_102172@163.com>
2013-07-08 00:09:01 -0700

    add twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '8ee608eabf03d93b0f4b7ca0f6530aea4fc640b2c1001951614de65ff81084585d423d11643bf527fafc6cf45a7f79b676c9a8108bf28f36537a8df81fbad71a'
